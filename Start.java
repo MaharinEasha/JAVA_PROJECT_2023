9d2aa6789642bb3135b59b344b03b86ad45c44ee
Maharun Binta Kibria Easha <maharineasha999@gmail.com>
2026-04-22 00:25:39 +0600

    Initial commit - Java Project 2023

---
+++ Start.java
@@ -0,0 +1,21 @@
+import Admin.*;
+import Customer.*;
+import loginframe.*;
+import Classes.*;
+import Admin.Garage;
+
+public class Start
+
+{
+    public static void main(String []args)
+    {
+      
+     new HomePage(); 
+	// new MobileBanking();
+	
+	
+	
+
+   }
+    
+}
---
+++ loginframe/Terms_and_Condition.java
@@ -0,0 +1,20 @@
+
+
+package loginframe;
+import javax.swing.*;
+
+
+public class Terms_and_Condition extends JFrame {
+    
+    protected JPanel panel;
+    protected JLabel label1,label2;
+    
+    public Terms_and_Condition()
+    {
+        label1 = new JLabel();
+        label2 = new JLabel();
+        
+    }
+    
+    
+}
